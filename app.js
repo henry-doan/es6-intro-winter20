4dc860f20bbeec14bc6afe3db9f836513004019e
Henry Doan <henrydoan96@gmail.com>
2020-12-08 20:51:51 -0700

    finished lecture

---
+++ app.js
@@ -0,0 +1,18 @@
+import printName, { PER_PAGE, MAX_USERS, DEFAULT_COLOR } from './notes';
+
+// packages or lib
+// var React = require('react');
+import React from 'react';
+
+// var React = require('react');
+// var component = React.Component;
+import React, { Component } from 'react';
+
+
+
+import { IndexRoute, Link, browserHistory } from 'react-router';
+
+
+// ..
+// ./
+// /
--- notes.js
+++ notes.js
@@ -75,3 +75,195 @@
 //   var xIsALie = true
 
 // no longer using var for variables
+
+// Let 
+// let first_name = 'bob'
+  // scoped to the nearest code block
+// first_name = 'mike'
+
+// const
+  // short constant, these values shouldn't be change
+  // read only 
+  // const MAX_USERS = 400
+  // MAX_USERS = 200
+
+  // Functions 
+  // function countColors(colors) {
+  //   return colors.length
+  // }
+  
+  // countColors();
+
+  // default params
+  // function countColors(colors = []) {
+  //   return colors.length
+  // }
+  
+  // console.log(countColors())
+  // console.log(countColors(["red", "white", "blue"]))
+
+  // function showUserInfo(name, options = {}) {
+  //   let likes = options.likes 
+  //   let followers = options.followers 
+  //   let following = options.following 
+  // }
+
+  // function showUserInfo(name, { likes, followers, following }) {
+  //   likes 
+  //   followers 
+  //   following 
+  // }
+
+  // showUserInfo("Spencer", { likes: 4, followers: 10, following: 13 });
+
+  // Rest Params 
+    // accept an infinite number or arguments,  ... name of reference of the rest of the params
+
+    // function showTrendingTags(...hashTags) {
+    //   for(let i in hashTags) {
+    //     displayTopic(hashTags[i]);
+    //   }
+    // }
+
+    // function logColors(...colors) {
+    //   for(let i in colors) {
+    //     console.log(colors[i])
+    //   }
+    // }
+    
+    // logColors()
+    // logColors('red', 'green')
+    // logColors('red', 'green', 'blue')
+    // logColors('red', 'green', 'blue', 'black', 'pink')
+
+
+    // function logColors(firstColor, ...colors) {
+    //   console.log(firstColor)
+    //   for(let i in colors) {
+    //     console.log(colors[i])
+    //   }
+    // }
+    
+    // logColors()
+    // logColors('red', 'green')
+    // logColors('red', 'green', 'blue')
+    // logColors('red', 'green', 'blue', 'black', 'pink')
+
+
+    // Spread Operators 
+    //   ... 
+    //     spread everything the object has 
+
+    // function getMovies("/movies", function(data) {
+    //   let movies = data.movies;
+    //   displayMovies(...movies);
+    // });
+
+    // jiggle all the way, santa claus, nightmare before christmas
+
+    // this.state = { name: 'bob', age: 23, email: 'adfasf@email.com' }
+    // this.state.name
+    // this.state.age
+    // this.state.email
+    // {...this.state}
+
+    // Functions 
+    // function hello(name) {
+
+    // }
+
+    // const hello => {
+
+    // }
+
+    // const hello => 
+
+    // const hello = (name) => {
+
+    // }
+
+    // hello = () => {
+
+    // }
+
+    // () => this.props.deleteContact
+
+
+    // Object Destructuring
+    // let officer = officerInfo("Clive", "Savacool", "Chief", "clive@fakeemail.com");
+
+    // let first = officer.first;
+    // let last = officer.last;
+    // let fullName = officer.fullName;
+    // let email = officer.email;
+    // let rank = officer.rank;
+
+    // let { first, last, fullName, email, rank } = officer
+
+    // this.state = { name: 'bob', age: 23, email: 'adfasf@email.com' }
+    // this.state.name
+    // this.state.age
+    // this.state.email
+    // let { name, age, email } = this.state
+
+    // Strings 
+    // "hello" + planet + '!'
+
+    // `hello ${planet}!`
+
+    // `
+    //   hello
+    //   world
+    // `
+
+    // let friends = ["Jake", "Brad", "Brandon", "Ethan", "Ryan"];
+    // for(index in friends) {
+    //   console.log(friends[index]);
+    // }
+
+    // let friends = ["Jake", "Brad", "Brandon", "Ethan", "Ryan"];
+    // for(name of friends) {
+    //   console.log(name);
+    // }
+
+    // 2 ** 3 
+    // to the power of 
+
+    // let users = [
+    //   { name: 'Dave', role: 'Instructor' },
+    //   { name: 'Brad', role: 'TA' },
+    //   { name: 'Parker', role: 'TA' },
+    //   { name: 'Sara', role: 'Student'},
+    //   { name: 'Jill', role: 'Student'},
+    //   { name: 'Greg', role: 'Student'}
+    // ];
+    
+    // let student = users.find( (user) => {
+    //   return user.role == "Student";
+    // });
+
+    // ??
+
+    // check null or undefined
+
+
+    // Promises
+    // new Promise resolved 
+
+    // .then( authStatus => { return getUser(authStatus.auth, 1); })
+    // .catch( err => console.log(err) )
+    // .done( )
+    // .finally( )
+
+    // async wait 
+
+    const PER_PAGE = 10;
+    const MAX_USERS = 25;
+    const DEFAULT_COLOR = 'red';
+
+    const printName = (name) => {
+      console.log(name)
+    }
+
+    export { PER_PAGE, MAX_USERS, DEFAULT_COLOR };
+    export default printName;
